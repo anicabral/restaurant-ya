0606ffd51421ef17c3cac060849e6a40f71d5f0f
ana cabral <cabralana18@gmail.com>
2023-12-27 09:25:05 -0300

    creacion modelo Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_27_122455) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_27_122505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
