70beea48bb4b325dd51ec16e9054b9471803b9f5
ana cabral <cabralana18@gmail.com>
2023-12-27 09:33:33 -0300

    Validaciones y asociaciones

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  # Asociaciones
+  has_many :category_restaurants
+  has_many :restaurants, through: :category_restaurants
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ApplicationRecord
-  belongs_to :user
+   # Asociaciones
+   belongs_to :user
+   has_many :category_restaurants
+   has_many :categories, through: :category_restaurants
+   has_one_attached :photo
+
+   # Validaciones
+   validates :name, :address, length: { minimum: 5 }
+   validates :description, length: {minimum: 15 }
 end
